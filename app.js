85111bc9301d4bba2bbc50e3c960c6a9967a4f08
Hernan1Escudero <hernanescudero2@gmail.com>
2023-06-30 11:00:37 -0300

    reparo repo

---
+++ app.js
@@ -0,0 +1,11 @@
+const express= require("express")
+const PORT =3030
+const app= express()
+const path= require("path")
+
+
+app.use(express.static('public'))
+app.get("/",(req,res)=> res.sendFile(path.join(__dirname,"views","home.html")))
+app.get("/home",(req,res)=> res.sendFile(path.join(__dirname,"views","home.html")))
+
+app.listen(PORT,()=>console.log("corriendo en ","localhost:"+PORT))
