bbcc0b7063fee386853af7c442ddbbb67f8fc96c
Hernan1Escudero <hernanescudero2@gmail.com>
2023-07-29 17:08:22 -0300

    "Fix: existia develop pero nunca se comiteaba ahi"

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const path= require("path")
 app.use(express.static('public'))
 app.get("/",(req,res)=> res.sendFile(path.join(__dirname,"views","home.html")))
 app.get("/home",(req,res)=> res.sendFile(path.join(__dirname,"views","home.html")))
+app.get("/register",(req,res)=> res.sendFile(path.join(__dirname,"views","register.html")))
 
 app.listen(PORT,()=>console.log("corriendo en ","localhost:"+PORT))
 
