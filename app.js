4a187c65ade61209779917446c6a822b2268c13d
Hernan1Escudero <hernanescudero2@gmail.com>
2023-07-29 21:12:32 -0300

    "Microdesafio formularios"

--- app.js
+++ app.js
@@ -7,8 +7,8 @@ const path= require("path")
 app.use(express.static('public'))
 app.get("/",(req,res)=> res.sendFile(path.join(__dirname,"views","home.html")))
 app.get("/home",(req,res)=> res.sendFile(path.join(__dirname,"views","home.html")))
+app.get("/header",(req,res)=> res.sendFile(path.join(__dirname,"views","partials","header.html")))
+app.get("/footer",(req,res)=> res.sendFile(path.join(__dirname,"views","partials","footer.html")))
 app.get("/register",(req,res)=> res.sendFile(path.join(__dirname,"views","register.html")))
 app.get("/login",(req,res)=> res.sendFile(path.join(__dirname,"views","login.html")))
 app.listen(PORT,()=>console.log("corriendo en ","localhost:"+PORT))
-
-// vamos a ver estos cambios
